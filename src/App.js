d13f01a30670fab82db9fa8d34124d2d4f5226c6
Mohammad Maninang <mohammadmaninang94@gmail.com>
2020-04-25 10:11:18 +0800

    upating our app with a shop page, collection-preview and collection-item components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </BrowserRouter>
     </div>
@@ -18,3 +20,4 @@ function App() {
 }
 
 export default App;
+
